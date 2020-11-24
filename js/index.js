b824ba7904db7c44c2234c1fab57ee777f8d3185
Ewald <66895372+EwaldNel@users.noreply.github.com>
2020-11-24 11:47:23 +0200

    Add files via upload
    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const navTOggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav-link");
+navTOggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open")
+})
+
+navLinks.forEach(link => {
+    link.addEventListener("click" , () => {
+        document.body.classList.remove("nav-open");
+    })
+})
