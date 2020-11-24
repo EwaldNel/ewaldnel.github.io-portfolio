b3412edfce81737620adffa4aa073b89b9c89b2b
EwaldNel <66895372+EwaldNel@users.noreply.github.com>
2020-11-24 18:40:09 +0200

    JavaScript Theme Function
    Added a JavaScript Function that allows me to Select a different theme for my site.
    
    Added HTMl for the content.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 baguetteBox.run('.portfolio');
+
+var themes = document.querySelectorAll(".themes span");
+var root = document.querySelector(":root");
+
+themes.forEach((theme) => {
+    theme.addEventListener("click", (e) => {
+        root.style.setProperty("--main-default-color", e.target.style.background);
+    })
+});
